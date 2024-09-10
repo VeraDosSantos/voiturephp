e81dcac3a10be507cee7b3893c6f90e5eb63d647
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-10 16:05:21 +0200

    creation du projet voiture

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    include_once(__DIR__ . '/partials/head.php');
+?>
+<?php 
+    include_once(__DIR__ . "/partials/footer.php") 
+?>
---
+++ partials/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Poles Voiture</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+        <img src="/public/img/logo.jpg" alt="Logo le Poles Voiture" width="30" height="30" class="d-inline-block align-text-top">
+        Le Poles Voiture
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="/">Home</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
